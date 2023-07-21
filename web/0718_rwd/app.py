d9f4e54fa6c4016e7b8962fa1e920de8f861652e
otischung <my072814638@gmail.com>
2023-07-21 13:05:43 +0800

    add flask api

--- web/0718_rwd/app.py
+++ web/0718_rwd/app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, request
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
+api = Api(app)
+
+
+class GetSummary(Resource):
+    def post(self):
+        print(request.get_json(force=True))
+        return {"status": "success"}
 
 
 @app.route("/")
 def root():
     return render_template("index.html")
+
+
+api.add_resource(GetSummary, "/summarypost")
 
 
 if __name__ == "__main__":
--- web/0718_rwd/static/js/app.js
+++ web/0718_rwd/static/js/app.js
@@ -22,3 +22,32 @@ $(document).ready(function () {
     // Call the autoResize function whenever the user types
     textarea.on('input', autoResize);
 });
+
+// This function maintains the ajax API for the summit button
+$("#getSummary").click(function () {
+    console.log("Button on click");  // For debugging
+    $.ajax({
+        method: "POST",
+        url: "/summarypost",
+        contentType: 'application/json',
+        data: JSON.stringify($("#inputTextBox").val()),
+        dataType: "json",
+    })
+    .done(function(result) {
+        console.log(result);
+        console.log(result["status"]);
+        alert('成功');
+    })
+    .fail(function() {
+        alert('失敗');
+    })
+    .always(function() {
+        alert('結束');
+    });
+});
+/*
+This means when you're sending JSON to the server or receiving JSON from the server,
+you should always declare the Content-Type of the header as application/json as this is
+the standard that the client and server understand.
+Ref: https://www.freecodecamp.org/news/what-is-the-correct-content-type-for-json-request-header-mime-type-explained/
+*/
