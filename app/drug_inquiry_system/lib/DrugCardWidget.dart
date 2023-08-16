e55eb4a2d36910fe721715e5da4a8769af7a8f29
otischung <my072814638@gmail.com>
2023-08-16 16:50:34 +0800

    add favorite

---
+++ app/drug_inquiry_system/lib/DrugCardWidget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class DrugCardWidget extends StatefulWidget {
+  const DrugCardWidget({
+    super.key,
+    required this.favoriteDrugNames,
+    required this.item,
+    required this.imgSrc,
+  });
+
+  final List<String> favoriteDrugNames;
+  final Map<String, dynamic> item;
+  final String imgSrc;
+
+  @override
+  State<DrugCardWidget> createState() => _DrugCardWidgetState();
+}
+
+class _DrugCardWidgetState extends State<DrugCardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: [
+          // 喜好愛心按鈕
+          IconButton(
+            onPressed: () {
+              // 狀態重設，重新繪製 UI
+              setState(() {
+                if (widget.favoriteDrugNames.contains(
+                    widget.item['中文品名'])) {
+                  widget.favoriteDrugNames.remove(
+                      widget.item['中文品名']);
+                } else {
+                  widget.favoriteDrugNames
+                      .add(widget.item['中文品名']);
+                }
+                debugPrint(
+                    widget.favoriteDrugNames.toString());
+              });
+            },
+            icon: (widget.favoriteDrugNames.contains(
+                widget.item['中文品名']) == true)
+                ? Icon(Icons.favorite)
+                : Icon(Icons.favorite_border)
+          ),
+          Expanded(
+            child: ListTile(
+              title: Text(widget.item['中文品名']),
+              subtitle: Text(widget.item['適應症']),
+            ),
+          ),
+          Image.network(
+            widget.imgSrc,
+            width: 100,
+            height: 100,
+          )
+        ],
+      ),
+    );
+  }
+}
--- app/drug_inquiry_system/lib/my_home_page.dart
+++ app/drug_inquiry_system/lib/my_home_page.dart
@@ -3,6 +3,8 @@ import 'package:drug_inquiry_system/drug_information_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
+import 'DrugCardWidget.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -13,6 +15,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String keyWord = "";
+  List<String> favoriteDrugNames = [];
+
   // 載入資料(DLI and DA)
   Future<Map<String, dynamic>> loadData() async {
     String jsonDLIString = await rootBundle.loadString('assets/data/DLI.json');
@@ -39,70 +44,103 @@ class _MyHomePageState extends State<MyHomePage> {
               .colorScheme
               .onPrimary,
           title: Text(widget.title),
+          actions: [
+            IconButton(
+                onPressed: (){},
+                icon: const Icon(Icons.favorite)
+            )
+          ],
         ),
         // 重要 FutureBuilder
-        body: FutureBuilder<Map<String, dynamic>>(
-          future: loadData(),
-          builder: (BuildContext context,
-              AsyncSnapshot<Map<String, dynamic>> snapshot) {
-            if (snapshot.hasData) {
-              final data = snapshot.data;
-
-              // 重要 ListView.builder
-              return ListView.builder(
-                itemCount: data!["DLI"].length,
-                itemBuilder: (BuildContext context, int index) {
-                  // 取得 圖片位址
-                  String imgSrc = "";
-                  bool containsKey =
-                  data!["DA"].containsKey(data['DLI'][index]['中文品名']);
-                  if (containsKey == true) {
-                    imgSrc = data['DA'][data['DLI'][index]['中文品名']];
-                    print(imgSrc);
-                  } else {
-                    imgSrc =
-                    "https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=2000";
-                  }
-
-                  // 重要 在App上要顯示的內容
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) =>
-                              DrugInformationPage(
-                                  data: data['DLI'][index],
-                                  imgSrc: imgSrc
-                              )
-                          )
-                      );
-                    },
-                    child: Card(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: ListTile(
-                              title: Text(data['DLI'][index]['中文品名']),
-                              subtitle: Text(data['DLI'][index]['適應症']),
-                            ),
-                          ),
-                          Image.network(
-                            imgSrc,
-                            width: 100,
-                            height: 100,
-                          )
-                        ],
-                      ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Search',
+                      prefixIcon: Icon(Icons.search),
                     ),
-                  );
+                    onChanged: (value) {
+                      keyWord = value;
+                    },
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        // 刷新
+                      });
+                    },
+                    icon: Icon(Icons.search))
+              ],
+            ),
+            Expanded(
+              child: FutureBuilder<Map<String, dynamic>>(
+                future: loadData(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<Map<String, dynamic>> snapshot) {
+                  if (snapshot.hasData) {
+                    final data = snapshot.data;
+
+                    // 判斷是否有點擊搜尋
+                    List<dynamic> newData = [];
+                    if (keyWord != "") {
+                      for (int i = 0; i < data!['DLI'].length; i++) {
+                        if (data['DLI'][i]['中文品名'].contains(keyWord)) {
+                          newData.add(data['DLI'][i]);
+                        }
+                      }
+                      data!['DLI'] = newData;
+                    }
+
+                    // 重要 ListView.builder
+                    return ListView.builder(
+                      itemCount: data!["DLI"].length,
+                      itemBuilder: (BuildContext context, int index) {
+                        // 取得 圖片位址
+                        String imgSrc = "";
+                        bool containsKey =
+                        data!["DA"].containsKey(data['DLI'][index]['中文品名']);
+                        if (containsKey == true) {
+                          imgSrc = data['DA'][data['DLI'][index]['中文品名']];
+                          print(imgSrc);
+                        } else {
+                          imgSrc =
+                          "https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=2000";
+                        }
+
+                        // 重要 在App上要顯示的內容
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) =>
+                                    DrugInformationPage(
+                                        data: data['DLI'][index],
+                                        imgSrc: imgSrc
+                                    )
+                                )
+                            );
+                          },
+                          child: DrugCardWidget(
+                            favoriteDrugNames: favoriteDrugNames,
+                            item: data['DLI'][index],
+                            imgSrc: imgSrc,
+                          ),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return CircularProgressIndicator();
+                  }
                 },
-              );
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              return CircularProgressIndicator();
-            }
-          },
+              ),
+            ),
+          ],
         ));
   }
 }
